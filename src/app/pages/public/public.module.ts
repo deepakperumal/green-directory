bbc33aecadb79ae98c92146a75749919b8eb5ee6
Debasish Sahoo <dsahoo@intouchtechnology.com>
2020-08-07 01:49:56 +0530

    added carousel slider

--- src/app/pages/public/public.module.ts
+++ src/app/pages/public/public.module.ts
@@ -6,20 +6,26 @@ import { FormsModule } from '@angular/forms'
 import { HomeComponent } from './home/home.component'
 import { AboutComponent } from './about/about.component'
 import { HeaderComponent } from './common/header.component'
-import { FooterComponent } from './common/footer.component'
+import { FooterComponent } from './common/footer.component';
+import { CarouselComponent } from './home/carousel/carousel.component'
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {CommonModule} from '@angular/common'
 
 @NgModule({
   imports: [
     PublicRoutingModule,
     RouterModule,
-    FormsModule 
+    FormsModule,
+    NgbModule,
+    CommonModule 
   ],
   declarations: [
     PublicComponent,
     HomeComponent,
     AboutComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    CarouselComponent
   ],
 })
 export class PublicModule {
