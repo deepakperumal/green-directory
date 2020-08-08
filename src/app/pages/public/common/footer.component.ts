59e15620486bdfceff0fb923b3189c793c31d11f
deepakperumal <a.deepakperumal@gmail.com>
2020-08-08 11:01:02 +0530

    added footer

--- src/app/pages/public/common/footer.component.ts
+++ src/app/pages/public/common/footer.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ngx-footer',
   templateUrl: './footer.component.html',
+  styleUrls: ['./common.component.scss']
 
 })
-export class FooterComponent {
+export class FooterComponent { 
 
 }
   
